a58dae96580c1d3f357680e97c5703f374c127bc
iffy-pi <59808927+iffy-pi@users.noreply.github.com>
2023-08-23 01:30:07 -0400

    Updated to remove text conversion

--- nutrition-framework/shortcuts/Log_Nutrients_To_Health.py
+++ nutrition-framework/shortcuts/Log_Nutrients_To_Health.py
@@ -14,65 +14,49 @@ nutrients = Dictionary( Text( GetFile(f'{storage}/Other/temp.txt') ) )
 loggingDate = Date(nutrients['Date'])
 
 if nutrients["Carbs"] is not None:
-	text = f'0{nutrients["Carbs"]}'
-    LogHealthSample("Carbohydrates", text, "g", loggingDate)
+    LogHealthSample("Carbohydrates", {nutrients["Carbs"]}, "g", loggingDate)
 
 if nutrients["Fiber"] is not None:
-	text = f'0{nutrients["Fiber"]}'
-    LogHealthSample("Fiber", text, "g", loggingDate)
+    LogHealthSample("Fiber", {nutrients["Fiber"]}, "g", loggingDate)
 
 if nutrients["Sugar"] is not None:
-	text = f'0{nutrients["Sugar"]}'
-    LogHealthSample("Dietary Sugar", text, "g", loggingDate)
+    LogHealthSample("Dietary Sugar", {nutrients["Sugar"]}, "g", loggingDate)
 
 if nutrients["Fat"] is not None:
-	text = f'0{nutrients["Fat"]}'
-    LogHealthSample("Total Fat", text, "g", loggingDate)
+    LogHealthSample("Total Fat", {nutrients["Fat"]}, "g", loggingDate)
 
 if nutrients["Polyunsaturated"] is not None:
-	text = f'0{nutrients["Polyunsaturated"]}'
-    LogHealthSample("Polyunsaturated Fat", text, "g", loggingDate)
+    LogHealthSample("Polyunsaturated Fat", {nutrients["Polyunsaturated"]}, "g", loggingDate)
 
 if nutrients["Monounsaturated"] is not None:
-	text = f'0{nutrients["Monounsaturated"]}'
-    LogHealthSample("Monounsaturated Fat", text, "g", loggingDate)
+    LogHealthSample("Monounsaturated Fat", {nutrients["Monounsaturated"]}, "g", loggingDate)
 
 if nutrients["Saturated"] is not None:
-	text = f'0{nutrients["Saturated"]}'
-    LogHealthSample("Saturated Fat", text, "g", loggingDate)
+    LogHealthSample("Saturated Fat", {nutrients["Saturated"]}, "g", loggingDate)
 
 if nutrients["Protein"] is not None:
-	text = f'0{nutrients["Protein"]}'
-    LogHealthSample("Protein", text, "g", loggingDate)
+    LogHealthSample("Protein", {nutrients["Protein"]}, "g", loggingDate)
 
 if nutrients["Sodium"] is not None:
-	text = f'0{nutrients["Sodium"]}'
-    LogHealthSample("Sodium", text, "mg", loggingDate)
+    LogHealthSample("Sodium", {nutrients["Sodium"]}, "mg", loggingDate)
 
 if nutrients["Potassium"] is not None:
-	text = f'0{nutrients["Potassium"]}'
-    LogHealthSample("Potassium", text, "mg", loggingDate)
+    LogHealthSample("Potassium", {nutrients["Potassium"]}, "mg", loggingDate)
 
 if nutrients["Cholesterol"] is not None:
-	text = f'0{nutrients["Cholesterol"]}'
-    LogHealthSample("Dietary Cholesterol", text, "mg", loggingDate)
+    LogHealthSample("Dietary Cholesterol", {nutrients["Cholesterol"]}, "mg", loggingDate)
 
 if nutrients["VitA"] is not None:
-	text = f'0{nutrients["VitA"]}'
-    LogHealthSample("Vitamin A", text, "mcg", loggingDate)
+    LogHealthSample("Vitamin A", {nutrients["VitA"]}, "mcg", loggingDate)
 
 if nutrients["VitC"] is not None:
-	text = f'0{nutrients["VitC"]}'
-    LogHealthSample("Vitamin C", text, "mg", loggingDate)
+    LogHealthSample("Vitamin C", {nutrients["VitC"]}, "mg", loggingDate)
 
 if nutrients["Calcium"] is not None:
-	text = f'0{nutrients["Calcium"]}'
-    LogHealthSample("Calcium", text, "mg", loggingDate)
+    LogHealthSample("Calcium", {nutrients["Calcium"]}, "mg", loggingDate)
 
 if nutrients["Iron"] is not None:
-	text = f'0{nutrients["Iron"]}'
-    LogHealthSample("Iron", text, "mg", loggingDate)
+    LogHealthSample("Iron", {nutrients["Iron"]}, "mg", loggingDate)
 
 if nutrients["Calories"] is not None:
-	text = f'0{nutrients["Calories"]}'
-    LogHealthSample("Dietary Energy", text, "kcal", loggingDate)
+    LogHealthSample("Dietary Energy", {nutrients["Calories"]}, "kcal", loggingDate)
