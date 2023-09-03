e52ccc6329acf290dc84e0bd068d4b0aa0863f59
iffy-pi <59808927+iffy-pi@users.noreply.github.com>
2023-09-03 03:06:22 -0400

    Made pyfile script but havent implemented it in the shortcuts app

--- nutrition-framework/shortcuts/Log_Foods_At_Different_Times.py
+++ nutrition-framework/shortcuts/Log_Foods_At_Different_Times.py
@@ -13,15 +13,7 @@ storage = Text(GetFile("Nutrition_Shortcut_Storage_Folder_Name.txt"))
 
 nutrDix = Dictionary(GetFile(f"{storage}/Other/shortcutNames.json"))
 
-# Get all the foods to log by running Foods_List.py
-foodsInfo = RunShortcut(nutrDix['Foods List'], input={ 'passToBulkEntry': True})
-
-# foodsDix maps unique list ID to each food
-# selectedIds contains the list IDs of the selected foods
-foodsDix = Dictionary(foodsInfo['foodsDix'])
-selectedIds = foodsInfo['selectedIds']
-
-hasNotes = FALSE
+hasFoodNotes = FALSE
 
 file = GetFile(f"{storage}/Other/foodNotes.txt", errorIfNotFound=False)
 if file is not None:
@@ -30,60 +22,128 @@ if file is not None:
         {file}
 
     '''
-    notes = IFRESULT
-    hasNotes = TRUE
+    hasFoodNotes = TRUE
 
-# Count the selected items
-for _ in range(Count(selectedIds)):
-    if Count(selectedIds) > 0:
-        # Generate food contact cards
-        for listId in selectedIds
-            food = foodsDix[listId]
-            text = f'''
-            BEGIN:VCARD
-            VERSION:3.0
-            N;CHARSET=UTF-8:{food['Name']}
-            ORG;CHARSET=UTF-8:{food['Servings']} servings
-            NOTE;CHARSET=UTF-8:{listId}
-            END:VCARD
-            '''
-            REPEATRESULTS.append(text)
 
-        contacts = textToContacts(REPEATRESULTS)
+# Each food item has a unique list ID which is different from their food id
+# For a given listId i, foodsInfo[i] is the food for that listId and
+# datesInfo[i] is the date and time for that list id
+foodsInfo = {}
+datesInfo = {}
+selectedIds = []
+breakLoop = FALSE
+nextId = 0
 
-        # If there are food notes, add them to the menu prompt so that users can see the time
-        if hasNotes == TRUE:
+for _ in range(30):
+    if breakLoop == FALSE:
+        # Generate a simple menu prompt listing all the foods added with their log times
+        if Count(selectedIds) > 0:
+            for listId in selectedIds:
+                food = foodsInfo[listId]
+                date = datesInfo[listId]
+
+                REPEATRESULTS.append(f'{food['Servings']}x {food['Name']} for {date.format(custom='h:mm a, MMM d')}')
+
             IFRESULT = f'''
-            {notes}
-
-            Select foods to log for time...
-            Foods will be logged after all foods have been selected.
+                Added Foods:
+                {REPEATRESULTS}
             '''
         else:
-            IFRESULT = 'Select foods to log for time...\nFoods will be logged after all foods have been selected.'
+            IFRESULT = 'No Foods Added'
 
-        chosen = ChooseFrom(contacts, prompt=IFRESULT, selectMultiple=True)
-        
-        # Ask for the date to log selected foods at time
-        date = AskForInput(Input.DateAndTime, "What time should the selected foods be logged at?")
+        basePrompt = IFRESULT
 
-        for item in chosen:
-            # Get the list ID from contact notes, filter it from the remaining items in the list
-            # Then use the ID to get the food and add it to items to log
-            listId = Contact(item).Notes
-            selectedIds = filter(selectedIds, where=['Name' == listId])
-            food = foodsDix[listId]
-            dix = Dictionary({
-                'Date': date.format(date="medium", time="short"),
-                'Food': food
-            })
-            logItems.append(dix)
+        if hasFoodNotes == TRUE:
+            IFRESULT = f'''
+                {notes}
 
-for item in logItems:
-    # Log the foods outside the loop, done to fast track user input
-    res = RunShortcut(nutrDix['Log Algorithm'], input=item)
+                {basePrompt}
+            '''
+        else:
+            IFRESULT = basePrompt
+
+        text = f'''
+            {IFRESULT}
+            Select An Option
+        '''
+
+        Menu(text):
+            case 'Log Added Foods':
+                # breaks loop, foods are logged outside of loop
+                breakLoop = TRUE
+
+            case 'Add Foods For Time':
+                # Asks user for the date, runs foods list and maps 
+                # the selected foods to the given date and time 
+                date = AskForInput(Input.DateAndTime, prompt='Select Date and Time')
+                for food in RunShortcut(nutrDix['Foods List']):
+                    foodsInfo[nextId] = food
+                    datesInfo[nextId] = date
+                    selectedIds.append(nextId)
+                    nextId = nextId+1
+
+            case 'Edit Times For Food...':
+                # use contact vcards to get the list ids of the foods the user wants to change
+                for listId in selectedIds:
+                    food = foodsInfo[listId]
+                    date = datesInfo[listId]
+                    text = f'''
+                        BEGIN:VCARD
+                        VERSION:3.0
+                        N;CHARSET=UTF-8:{food['Name']}
+                        ORG;CHARSET=UTF-8:{food['Servings']} servings ⸱ {date.format(custom='h:mm a, MMMM d')}
+                        NOTE;CHARSET=UTF-8:{listId}
+                        END:VCARD
+                    '''
+                    REPEATRESULTS.append(text)
+
+                contacts = textToContacts(REPEATRESULTS)
+
+                selectedContacts = ChooseFromList(contacts, prompt='Select Foods To Edit', selectMultiple=True)
+                
+                # for each of the foods, use the input prompt to get the new date and time
+                for contact in selectedContacts:
+                    listId = contact.Notes
+                    food = foodsInfo[listId]
+                    date = datesInfo[listId]
+                    date = AskForInput(Input.Date, prompt=f'Select new date and time for {food['Servings']}x {food['Name']}', default=date)
+                    datesInfo[listId] = date
+
+            case 'Remove Added Food...':
+                # use contact vcards to get the list ids of the foods the user wants to remove
+                for listId in selectedIds:
+                    food = foodsInfo[listId]
+                    date = datesInfo[listId]
+                    text = f'''
+                        BEGIN:VCARD
+                        VERSION:3.0
+                        N;CHARSET=UTF-8:{food['Name']}
+                        ORG;CHARSET=UTF-8:{food['Servings']} servings ⸱ {date.format(custom='h:mm a, MMMM d')}
+                        NOTE;CHARSET=UTF-8:{listId}
+                        END:VCARD
+                    '''
+                    REPEATRESULTS.append(text)
+
+                contacts = textToContacts(REPEATRESULTS)
+
+                selectedContacts = ChooseFromList(contacts, prompt='Select Foods To Remove', selectMultiple=True)
+                
+                # remove them from the selected Ids list
+                for contact in selectedContacts:
+                    listId = contact.Notes
+                    selectedIds = filter(selectedIds, where=['Name' == listId])
+
+            case 'Cancel and Exit':
+                StopShortcut()
+
+# log the foods in the selected Ids list
+for listId in selectedIds:
+    food = foodsInfo[listId]
+    date = datesInfo[listId]
+    res = RunShortcut(nutrDix['Log Algorithm'], input={'Date': date, 'Food': food})
     loggedFoods.append(res)
 
+# clearing foods notes and making preset
 if hasNotes == TRUE:
     Menu('Clear food notes?'):
         case 'Yes':
