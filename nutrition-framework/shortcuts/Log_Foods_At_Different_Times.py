2a59404d681c6a5de3c3eef8fc1f42350523449d
iffy-pi <59808927+iffy-pi@users.noreply.github.com>
2023-09-04 05:02:04 -0400

    #25: More changes made to source

--- nutrition-framework/shortcuts/Log_Foods_At_Different_Times.py
+++ nutrition-framework/shortcuts/Log_Foods_At_Different_Times.py
@@ -67,58 +67,37 @@ for _ in range(30):
             '''
         text = f'''
             Foods with ⚠ have no log time.
-            
+
             {prompt}
         '''
+        setLogTimes = FALSE
+        fromEditOption = FALSE
         Menu(text):
             case 'Log Added Foods':
                 # breaks loop, foods are logged outside of loop
                 breakLoop = TRUE
 
             case 'Add Foods':
+                # unset Ids tracks the list ids of foods that dont have a log time since they were just added
+                # it is used later on
+                unsetIds = []
                 for food in RunShortcut(nutrDix['Foods List']):
                     foodsInfo[nextId] = food
                     datesInfo[nextId] = date
                     selectedIds.append(nextId)
+                    # adding food to list of foods that dont have a log time
+                    unsetIds.append(nextId)
                     nextId = nextId+1
 
+                # run set log time interface
+                setLogTimes = TRUE
+
             case 'Set/Edit Log Time For Foods...':
-                # use contact vcards to get the list ids of the foods the user wants to change
-                for listId in selectedIds:
-                    food = foodsInfo[listId]
-                    date = datesInfo[listId]
-                    warning = ''
-
-                    if date is not None:
-                        datePrompt = f'Log Time: {date.format(custom='h:mm a, MMMM d')}'
-                    else:
-                        datePrompt = 'No Log Time Added ⚠'
-
-                    text = f'''
-                        BEGIN:VCARD
-                        VERSION:3.0
-                        N;CHARSET=UTF-8:{food['Name']}{warning}
-                        ORG;CHARSET=UTF-8:{food['Servings']} servings ⸱ Log Time: {date.format(custom='h:mm a, MMMM d')}
-                        NOTE;CHARSET=UTF-8:{listId}
-                        END:VCARD
-                    '''
-                    REPEATRESULTS.append(text)
-
-                contacts = textToContacts(REPEATRESULTS)
-
-                selectedContacts = ChooseFromList(contacts, prompt='Select Foods To Edit', selectMultiple=True)
-
-                if Count(selectedContacts) > 0:
-
-                    contact = selectedContacts.getFirstItem()
-                    date = datesInfo[contact.Notes]
-                    date = AskForInput(Input.DateAndTime, prompt=f'Select log time for selected foods', default=date)
-
-                    
-                    # for each of the foods, set the date that the user selected
-                    for contact in selectedContacts:
-                        listId = contact.Notes
-                        datesInfo[listId] = date
+                setLogTimes = TRUE
+                # since user is requesting to edit the items
+                # we make all the foods changeable by setting unsetIds to the full selectedIds list
+                unsetIds = selectedIds
+                fromEditOption = TRUE
 
             case 'Remove Added Food...':
                 # use contact vcards to get the list ids of the foods the user wants to remove
@@ -149,10 +128,73 @@ for _ in range(30):
                 # remove them from the selected Ids list
                 for contact in selectedContacts:
                     listId = contact.Notes
-                    selectedIds = filter(selectedIds, where=['Name' == listId])
+                    selectedIds = filter(selectedIds, where=['Name' != listId])
 
             case 'Cancel and Exit':
                 StopShortcut()
+
+        if setLogTimes == TRUE:
+            breakEditLoop = FALSE
+            # unsetIds is used here so that:
+            # when foods are added, users are immediately prompted to select the time for the added foods
+            # when user wants to edit food times (fromEditOption == TRUE), all the foods are shown (since unsetIds is set to selectedIds)
+            for _ in Count(unsetIds):
+                # break loop when there are no more foods with unset log times
+                if Count(unsetIds) == 0:
+                    breakEditLoop = TRUE
+
+                if breakEditLoop == FALSE:
+                    # use contact vcards to get the list ids of the foods the user wants to change
+                    for listId in unsetIds:
+                        food = foodsInfo[listId]
+                        date = datesInfo[listId]
+                        warning = ''
+
+                        if date is not None:
+                            datePrompt = f'Log Time: {date.format(custom='h:mm a, MMMM d')}'
+                        else:
+                            datePrompt = 'No Log Time Added ⚠'
+
+                        text = f'''
+                            BEGIN:VCARD
+                            VERSION:3.0
+                            N;CHARSET=UTF-8:{food['Name']}{warning}
+                            ORG;CHARSET=UTF-8:{food['Servings']} servings ⸱ Log Time: {date.format(custom='h:mm a, MMMM d')}
+                            NOTE;CHARSET=UTF-8:{listId}
+                            END:VCARD
+                        '''
+                        REPEATRESULTS.append(text)
+
+                    contacts = macros.textToContacts(REPEATRESULTS)
+
+                    selectedContacts = ChooseFromList(contacts, prompt='Select Foods To Set/Edit Log Time', selectMultiple=True)
+
+                    if Count(selectedContacts) > 0:
+                        contact = selectedContacts.getFirstItem()
+                        date = datesInfo[contact.Notes]
+
+                        for contact in selectedContacts:
+                            food = foodsInfo[contact.Notes]
+                            REPEATRESULTS.append(f'{food['Servings']}x {food['Name']}')
+
+                        text = f'''
+                            Select log time for:
+                            {REPEATRESULTS}
+                        '''
+
+                        date = AskForInput(Input.DateAndTime, prompt=text, default=date)
+
+                        # for each of the foods, set the date that the user selected
+                        # also remove from unset ids, so the loop will break if user sets log times for all unset foods
+                        for contact in selectedContacts:
+                            listId = contact.Notes
+                            datesInfo[listId] = date
+                            unsetIds = filter(unsetIds, where=['Name' != listId])
+
+                    # if user specifically requested to edit food times, then break immediately after one iteration
+                    if fromEditOption == TRUE:
+                        breakEditLoop = TRUE
+
 
 # log the foods in the selected Ids list
 for listId in selectedIds:
