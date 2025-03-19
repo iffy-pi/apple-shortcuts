bc3bcecea32498ae1ac570792582d6ba8c5e2683
iffy-pi <59808927+iffy-pi@users.noreply.github.com>
2025-03-19 13:09:05 -0400

    #78 - Added option to make and return

--- projects/nutrition-framework/shortcuts/Make_Food_Manually.py
+++ projects/nutrition-framework/shortcuts/Make_Food_Manually.py
@@ -6,6 +6,64 @@ Ver: 1.1
 
 # Create a food by manually filling in the nutrient fieldds
 
+noteTemplate = '''
+Populate the food information below. When finished return to the Shortcuts app for the Shortcut to continue.
+
+Name: 
+Barcode: 
+Serving Size: 
+Calories (kCal): 
+Fat (g): 
+Saturated Fat (g): 
+Trans Fat (g): 
+Carbs (g): 
+Fiber (g): 
+Sugar (g): 
+Protein (g): 
+Cholesterol (mg): 
+Sodium (mg): 
+Calcium (mg or %): 
+Iron (mg or %): 
+Potassium (mg or %): 
+VitA (mcg or %): 
+VitC (mg or %): 
+Monounsaturated Fat (g): 
+Polyunsaturated Fat (g): 
+'''
+
+display = {
+    "Name":               "Name",
+    "Barcode":            "Barcode",
+    "Serving Size":       "Serving Size",
+    "Calories (kCal)":    "Calories",
+    "Carbs (g)":              "Carbs",
+    "Protein (g)":              "Protein",
+    "Fat (g)":              "Fat",
+    "Sugar (g)":              "Sugar",
+    "Fiber (g)":              "Fiber",
+    "Monounsaturated Fat (g)":              "Monounsaturated",
+    "Polyunsaturated Fat (g)":              "Polyunsaturated",
+    "Saturated Fat (g)":              "Saturated",
+    "Cholesterol (mg)":              "Cholesterol",
+    "Trans Fat (g)":              "Trans",
+    "Sodium (mg)":              "Sodium",
+    "Potassium (mg or %)":              "Potassium",
+    "VitA (mcg or %)":              "VitA",
+    "VitC (mg or %)":              "VitC",
+    "Calcium (mg or %)":              "Calcium",
+    "Iron (mg or %)":              "Iron"
+}
+
+
+noteTitle = 'Nutrition Food Importer'
+
+
+prompt = '''
+How would you like to make it?
+Make immediately - You will be presented with a dictionary to fill in all the information about the food item
+Make and return - A new note will be created in your Notes app to fill the item. Once you're finished, return to the shortcut to resume execution
+'''
+
 storage = Text(GetFile(From='Shortcuts', "Nutrition_Shortcut_Storage_Folder_Name.txt"))
 Strings = Dictionary(GetFile(From='Shortcuts', f"{storage}/Other/gui_strings.json"))
 nutrDix = Dictionary(GetFile(From='Shortcuts', f"{storage}/Other/shortcutNames.json"))
@@ -16,8 +74,52 @@ exactValDix = {
     'Iron': 14
 }
 
+newFood = Dictionary(Text('{"Serving Size":"","Protein":0,"Trans":0,"Barcode":"0","Cholesterol":0,"Name":"","Sugar":0,"Monounsaturated":0,"Polyunsaturated":0,"Fat":0,"Fiber":0,"VitC":0,"Calories":0,"Iron":0,"VitA":0,"Potassium":0,"Saturated":0,"Sodium":0,"Calcium":0,"Carbs":0}'))
+
+Menu(prompt='How would you like to make it?'):
+    case 'Make immediately': # TODO workshop these!!
+        pass
+    
+    case 'Make and return':
+        if (note := FindNotes('All Notes', where=['Name' == noteTitle])) is not None:
+            DeleteNotes(note)
+
+
+        note = CreateNote(contents=noteTemplate, name=noteTitle)
+        OpenNote(note)
+
+        # Wait for user to come back
+        WaitToReturn()
+
+        note = Text(FindNotes('All Notes', where=['Name' == noteTitle], limit=1))
+        lines = SplitText(note, ByNewLines=True)
+
+        for line in lines:
+            parts = SplitText(line, custom=': ')
+
+            if Count(parts) > 1:
+                displayName = TrimWhitespace(GetItemsFromList(part, index=1))
+                nutrKey = display.GetDictionaryValue(displayName)
+                
+                if nutrKey is not None:
+                    rem = TrimWhiteSpace( CombineText(GetItemsFromList(startIndex=2), custom=': ') )
+                    
+                    if nutrKey == 'Name' or nutrKey == 'Barcode' or nutrKey == 'Serving Size':
+                        IFRESULT = rem
+                    else:
+                        if f'"{rem}"' == ""
+                            IFRESULT2 = 0
+                        else:
+                            IFRESULT2 = GetNumbersFromInput(rem)
+                        IFRESULT = IFRESULT2
+
+                    newFood[nutrKey] = IFRESULT
+
+
+
+
 # Run display food item to allow user to input values
-foodDix = RunShortcut(nutrDix['Display Food Item'])
+foodDix = RunShortcut(nutrDix['Display Food Item'], input=newFood)
 
 # If there are any vitamins or minerals, we ask if the value is Exact Values or Daily Percentages
 
